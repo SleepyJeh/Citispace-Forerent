c4ab7d4752d761756292ff90e2067f6a8142884f
rey <abrerajohnrey@gmail.com>
2025-10-15 16:57:15 +0800

    Layout setup

---
+++ app/Livewire/Actions/LoginForm.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Livewire\Actions;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+
+
+class LoginForm extends Component
+{
+    public $email = '';
+    public $password = '';
+    public $remember = false;
+
+    protected $rules = [
+        'email' => 'required|email',
+        'password' => 'required|min:6',
+    ];
+
+    public function updated($propertyName)
+    {
+        // Real-time validation
+        $this->validateOnly($propertyName);
+    }
+
+    public function login()
+    {
+        $this->validate();
+
+        $credentials = [
+            'email' => $this->email,
+            'password' => $this->password,
+        ];
+
+        if (Auth::attempt($credentials, $this->remember)) {
+            session()->flash('success', 'Login successful!');
+            return redirect()->to('/dashboard'); // Redirect to dashboard
+        }
+
+        session()->flash('error', 'Invalid email or password.');
+    }
+    public function render()
+    {
+        return view('livewire.forms.login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,30 +5,15 @@ use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('common/index');
 })->name('home');
+
+Route::get('/user-login', function () {
+    return view('common/login');
+})->name('user-login');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
-    Volt::route('settings/password', 'settings.password')->name('password.edit');
-    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
-
-    Volt::route('settings/two-factor', 'settings.two-factor')
-        ->middleware(
-            when(
-                Features::canManageTwoFactorAuthentication()
-                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
-                ['password.confirm'],
-                [],
-            ),
-        )
-        ->name('two-factor.show');
-});
 
 require __DIR__.'/auth.php';
