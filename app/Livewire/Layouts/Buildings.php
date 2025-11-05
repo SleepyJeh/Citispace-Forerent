7101786e5b4a26aabb0b789356cb19a277af8ccc
reigne <riverareigne20202020@gmail.com>
2025-11-05 12:45:06 +0800

    local

--- app/Livewire/Layouts/Buildings.php
+++ app/Livewire/Layouts/Buildings.php
@@ -6,12 +6,20 @@ use Livewire\Component;
 
 class Buildings extends Component
 {
+<<<<<<< HEAD
     public string $image = "images/image.png";
     public string $title = "";
     public string $address = "";
 
     public function render()
     {
+=======
+    public Property $property; // <-- Accept the Property model
+    // public $image; // You might need this if you add images
+    public function render()
+    {
+        // Access data like $this->property->building_name in the view
+>>>>>>> 5289b26 (local)
         return view('livewire.layouts.buildingcard');
     }
 }
